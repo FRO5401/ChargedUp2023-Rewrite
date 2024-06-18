1bb47cebe8d21697711dd9f24b6f71fb9a2a7f0e
Sophia <111466302+MintyLuve@users.noreply.github.com>
2024-06-18 19:39:55 -0400

    Constants file

---
+++ ChargedUp2023/src/main/java/frc/robot/Constants.java
@@ -0,0 +1,23 @@
+package frc.robot;
+
+public final class Constants {
+    public static class ControlConstants{
+
+    }
+
+    public static class DriveConstants{
+        
+    }
+    
+    public static class ArmConstants{
+        
+    }
+    
+    public static class ClawConstants{
+        
+    }
+    
+    public static class LEDConstants{
+        
+    }
+}
