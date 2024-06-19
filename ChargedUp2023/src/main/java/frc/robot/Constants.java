3946f352ac7e832d038471b274497e77c56b6331
Sophia <111466302+MintyLuve@users.noreply.github.com>
2024-06-19 16:53:39 -0400

    fix merge bugs

--- ChargedUp2023/src/main/java/frc/robot/Constants.java
+++ ChargedUp2023/src/main/java/frc/robot/Constants.java
@@ -2,10 +2,7 @@ package frc.robot;
 
 public final class Constants {
     public static class ControlConstants{
-        public static final int XBOX_CONTROLLER_DRIVER = 0;
-        public static final int XBOX_CONTROLLER_OPERATOR = 1;
-
-        //
+        // controller specs
         public static final int XBOX_CONTROLLER_DRIVER = 0;
         public static final int XBOX_CONTROLLER_OPERATOR = 1;
         public static final double CONTROLLER_SENSITIVITY = 0.05;
@@ -42,7 +39,7 @@ public final class Constants {
     }
 
     public static class CompressorConstants{
-        
+        //compressor id
         public static final int COMPRESSOR_ID = 0;
     }
 }
--- ChargedUp2023/src/main/java/frc/robot/Subsystems/Compresor.java
+++ ChargedUp2023/src/main/java/frc/robot/Subsystems/Compresor.java
@@ -6,7 +6,6 @@ package frc.robot.Subsystems;
 
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
-import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.CompressorConstants;
 
