da28cdfe51bf6bfc8b4490f7803759ea13128cea
Sophia <111466302+MintyLuve@users.noreply.github.com>
2024-07-02 18:49:17 -0400

    Taking out unused imports

--- ChargedUp2023/src/main/java/frc/robot/Subsystems/Claw.java
+++ ChargedUp2023/src/main/java/frc/robot/Subsystems/Claw.java
@@ -6,7 +6,6 @@ package frc.robot.Subsystems;
 
 /*  Imports */
 //    WPI
-import edu.wpi.first.wpilibj.PneumaticsControlModule;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
